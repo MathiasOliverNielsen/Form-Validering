65e0cdc82481a6c5ecab4fd233aff4802e64e206
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2025-03-21 10:13:00 +0100

    thankYouMessage display none

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,49 +10,53 @@ const fields = [
 // Get the userForm element and add an event listener to prevent the default form submission
 document.getElementById('userForm').addEventListener('submit', function (e) {
   e.preventDefault();
-});
 
-// Global variables
-let isValid = true;
-let errorMessages = [];
-let errorMessagesList = document.getElementById('errorMessages');
-let errorFooter = document.getElementById('errorFooter');
-let thankYouMessage = document.getElementById('thankYouMessage');
+  // Global variables
+  let isValid = true;
+  let errorMessages = [];
+  let errorMessagesList = document.getElementById('errorMessages');
+  let errorFooter = document.getElementById('errorFooter');
+  let thankYouMessage = document.getElementById('thankYouMessage');
 
-// clear the error messages in list
-errorMessagesList.innerHTML = '';
+  // clear the error messages in list
+  errorMessagesList.innerHTML = '';
 
-// reset error state
-document.querySelectorAll('input').forEach((input) => input.classList.remove('error-border'));
+  // reset error state
+  document.querySelectorAll('input').forEach((input) => input.classList.remove('error-border'));
 
-// validation function
-function validateForm(id, regex, message) {
-  let input = document.getElementById(id);
-
-  if (!regex.test(input.value)) {
-    input.classList.add('error-border');
-    console.log(message);
-    errorMessages.push(message);
-    isValid = false;
+  // validation function
+  function validateForm(id, regex, message) {
+    let input = document.getElementById(id);
+    console.log(input.value);
+    if (!regex.test(input.value)) {
+      input.classList.add('error-border');
+      console.log(message);
+      errorMessages.push(message);
+      isValid = false;
+    } else {
+      input.classList.add('success-border');
+      input.classList.remove('error-border');
+      console.log(`${id} - Validering korrekt`);
+    }
   }
-}
 
-// validate fields
-fields.forEach((field) => {
-  validateForm(field.id, field.regex, field.message);
-});
-
-// if valid hide form, show ty message
-if (isValid) {
-  document.getElementById('userForm').style.display = 'none';
-  thankYouMessage.style.display = 'block';
-  errorFooter.style.display = 'none';
-} else {
-  // if not valid, show error messages
-  errorMessages.forEach((message) => {
-    let li = document.createElement('li');
-    li.appendChild(document.createTextNode(message));
-    errorMessagesList.appendChild(li);
+  // validate fields
+  fields.forEach((field) => {
+    validateForm(field.id, field.regex, field.message);
   });
-  log('form has errors');
-}
+
+  // if valid hide form, show ty message
+  if (isValid) {
+    document.getElementById('userForm').style.display = 'none';
+    thankYouMessage.style.display = 'block';
+    errorFooter.style.display = 'none';
+  } else {
+    // if not valid, show error messages
+    errorMessages.forEach((message) => {
+      let li = document.createElement('li');
+      li.appendChild(document.createTextNode(message));
+      errorMessagesList.appendChild(li);
+    });
+    console.log('form has errors');
+  }
+});
